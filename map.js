5303129c435fc8c451980b5876589affe76e1c7b
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2017-01-21 01:01:04 +0300

    Add webgl validate

--- map.js
+++ map.js
@@ -44,7 +44,7 @@ map.on('click', function(e) {
 // Init
 function init() {
   if (!webgl_support()) {
-    alert('Включите WebGL или обновите браузер');
+    alert('Возможно, приложение работает некорректно. Включите WebGL или обновите браузер');
   }
   var _locations = [
     [59.936802, 30.327609],
