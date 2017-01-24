f02d512072136488b1d0ab9741e2f5585627e143
Sasha Konyshev <sasha.konyshev1003@gmail.com>
2017-01-24 19:32:54 +0300

    Prepare for night mode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // var baseURL = 'http://127.0.0.1:12345/api/v1';
 baseURL = 'https://susanin.ml/api/v1';
 // var baseURL_route = 'http://127.0.0.1/api';
-// baseURL_route = 'http://192.168.0.113/api';
+// baseURL_route = 'http://192.168.1.64/api';
 baseURL_route = 'https://susanin.ml/api';
 var djangoURL = 'https://susanin.ml/api/django';
 var TIMEOUT = 200;
